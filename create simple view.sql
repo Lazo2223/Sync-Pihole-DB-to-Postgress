7423b510f050b571dd0723a2803db3930baca40c
Lazo2223 <d.lazarov2223@gmail.com>
2025-05-22 19:15:25 +0300

    Update create simple view.sql

--- create simple view.sql
+++ create simple view.sql
@@ -1,31 +1,12 @@
 --Join tables for easy access and query only view_domains_info_all
 CREATE OR REPLACE VIEW view_domains_info_all AS
-
-SELECT did.domain, 
-        cid.ip, 
-        qst.status, 
-        qst.details
+SELECT did.domain, cid.ip, qst.status, qst.details, qs.timestamp
 FROM domain_by_id did
 LEFT JOIN query_storage qs
-	      ON qs.domain = did.id
+	ON qs.domain = did.id
 LEFT JOIN client_by_id cid
-	      ON cid.id = qs.client
+	ON cid.id = qs.client
 	LEFT JOIN query_status qst
-	      ON qst.id = qs.status
+	ON qst.id = qs.status
 
-SELECT * 
-FROM view_domains_info_all
-
--- first seen domain in the last 10 days
-	
-CREATE OR REPLACE VIEW new_activity_10_days AS
-SELECT domain, TO_TIMESTAMP(MIN(timestamp)) AS first_seen
-FROM view_domains_info_all
-GROUP BY domain
-HAVING MIN(timestamp) >= EXTRACT(EPOCH FROM NOW() - INTERVAL '10 day')
-
--- first seen domain in the last 24 hours
-SELECT domain, TO_TIMESTAMP(MIN(timestamp)) AS first_seen
-FROM view_domains_info_all
-GROUP BY domain
-HAVING MIN(timestamp) >= EXTRACT(EPOCH FROM NOW() - INTERVAL '1 day')
+SELECT * FROM view_domains_info_all
